f84d4e529b9fc2383bcec99df0a17a246e115a98
Sean Pollock <seanpollock3344@gmail.com>
2016-10-02 12:05:40 -0500

    adding stream set to keep track of connections

--- p2p_chat/peer.js
+++ p2p_chat/peer.js
@@ -1,11 +1,23 @@
+// import libraries
+var net = require('net');
 var topology = require('fully-connected-topology');
+var streamSet = require('stream-set');
+
+// get username from args
+var user_name = process.argv[2];
 
 // get peers from args
-var me = process.argv[2];
-var peers = process.argv.slice(3);
+var me = process.argv[3];
+var peers = process.argv.slice(4);
+
+// set up list of sockets
+var activeSockets = streamSet();
 
 var swarm = topology(me, peers);
 
 swarm.on('connection', (connection, peer) => {
+  activeSockets.add(connection);
+  console.log('made it here');
   console.log('new connection from ', peer);
+  console.log(activeSockets.size);
 });
