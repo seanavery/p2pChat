cc0bfc7c9f195cf28160b7276317791804d308dd
Sean Pollock <seanpollock3344@gmail.com>
2016-10-02 12:24:25 -0500

    process input and send to connections as json objects-- using duplex-json-stream

--- p2p_chat/peer.js
+++ p2p_chat/peer.js
@@ -2,9 +2,10 @@
 var net = require('net');
 var topology = require('fully-connected-topology');
 var streamSet = require('stream-set');
+var jsonStream = require('duplex-json-stream');
 
 // get username from args
-var user_name = process.argv[2];
+var user_name = process.argv[2].toString().trim();
 
 // get peers from args
 var me = process.argv[3];
@@ -16,8 +17,20 @@ var activeSockets = streamSet();
 var swarm = topology(me, peers);
 
 swarm.on('connection', (connection, peer) => {
+  connection = jsonStream(connection);
   activeSockets.add(connection);
-  console.log('made it here');
   console.log('new connection from ', peer);
-  console.log(activeSockets.size);
+  connection.on('data', (data) => {
+    console.log(data.username + '> ' + data.message);
+  });
+});
+
+process.stdin.on('data', (data) =>  {
+  var message = data.toString().trim();
+  activeSockets.forEach((socket) => {
+    socket.write({
+      username : user_name,
+      message : message
+    });
+  });
 });
